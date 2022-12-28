f6559a0e42e405c5bd23deb4e823f0ac73f9a7de
LeoH10462 <jhu27@go.pasadena.edu>
2022-12-27 16:15:52 -0800

    add bol class to gui, but failed

--- data/data_bol.py
+++ data/data_bol.py
@@ -13,3 +13,29 @@ class Bol:
 
     def setAmazon(self, number):
         amazon = item()
+
+    def setContainer(self, contain):
+        self.Container = contain
+
+    def setmbl(self, mbl):
+        self.MBL = mbl
+
+    def setEta(self, eta):
+        self.ETA = eta
+    
+    def setTruck(self, truck):
+        self.Truck = truck
+
+    def setCustomer(self, custom):
+        self.Customer = custom
+
+    def setNote(self, note):
+        self.Note = note
+
+    def printInof(self):
+        print("Customer: ["+ self.Customer + "]"
+                "\nETA: ["+ self.ETA +"]"
+                "\nMBL: ["+ self.MBL +"]"
+                "\nContainer: ["+ self.Container + "]"
+                "\nTruck: [" + self.Truck + "]"
+                "\nNote: [" + self.Note + "]")
